6fc716dc3530c7e0068ee8fc3ef559679d313566
Ali Al Qahtani <73781540+alijumaan@users.noreply.github.com>
2022-12-30 09:03:24 +0300

    edit blade files

--- resources/views/admin/exams/create.blade.php
+++ resources/views/admin/exams/create.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Exam (create)') }}
+            {{ __('global.create_exam') }}
         </h2>
     </x-slot>
 
--- resources/views/admin/students/addToClassroom.blade.php
+++ resources/views/admin/students/addToClassroom.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Students (add to class)') }}
+            {{ __('global.add_student_in_classroom') }}
         </h2>
     </x-slot>
 
