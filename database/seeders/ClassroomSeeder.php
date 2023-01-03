346a01c5cd824ee09557da0029288a20ea5c7c3d
Ali Al Qahtani <73781540+alijumaan@users.noreply.github.com>
2023-01-03 22:18:03 +0300

    edit database seeder

--- database/seeders/ClassroomSeeder.php
+++ database/seeders/ClassroomSeeder.php
@@ -2,9 +2,8 @@
 
 namespace Database\Seeders;
 
-use App\Enums\RoleEnum;
+use App\Enums\ClassYearEnum;
 use App\Models\Classroom;
-use App\Models\ClassYear;
 use App\Models\Lesson;
 use App\Models\User;
 use Illuminate\Database\Seeder;
@@ -18,19 +17,27 @@ class ClassroomSeeder extends Seeder
      */
     public function run()
     {
-        for ($i=1; $i <= User::student()->count(); $i++) {
+        $firstClassYearStudentCount = User::student()
+            ->where('class_year_id', ClassYearEnum::FIRST->value)
+            ->count();
+
+        for ($i=1; $i <= $firstClassYearStudentCount; $i++) {
             $classroom = Classroom::create([
                 'name_en' => 'Lap '.$i,
                 'name_ar' => 'الفصل  '.$i,
                 'lesson_id' => Lesson::all()->random()->id,
                 'teacher_id' => User::teacher()->get()->random()->id,
-                'class_year_id' => ClassYear::all()->random()->id,
+                'class_year_id' => ClassYearEnum::FIRST->value,
                 'location' => 'Floor '.$i.' room NO.'.$i
             ]);
 
-            $studentId = User::student()->get()->random()->id;
+            $studentId = User::student()
+                ->where('class_year_id', ClassYearEnum::FIRST->value)
+                ->get()
+                ->random()
+                ->id;
 
-            $classroom->users()->attach($studentId);
+            $classroom->students()->attach($studentId);
         }
     }
 }
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -18,7 +18,7 @@ class UserSeeder extends Seeder
     public function run()
     {
         User::create([
-            'full_name' => 'ادمن',
+            'full_name' => 'Admin',
             'phone' => '0566207808',
             'national_id' => 123456789,
             'email' => 'admin@admin.com',
@@ -31,10 +31,10 @@ class UserSeeder extends Seeder
             'remember_token' => Str::random(10),
         ]);
 
-        User::factory(50)->create(['class_year_id' => ClassYearEnum::FIRST->value]);
-        User::factory(50)->create(['class_year_id' => ClassYearEnum::SECONDE->value]);
-        User::factory(50)->create(['class_year_id' => ClassYearEnum::THIRD->value]);
-        User::factory(10)->create(['role_id' => RoleEnum::TEACHER->value]);
-        User::factory(150)->create(['role_id' => RoleEnum::PARENT->value]);
+        User::factory(10)->create(['class_year_id' => ClassYearEnum::FIRST->value]);
+        User::factory(10)->create(['class_year_id' => ClassYearEnum::SECONDE->value]);
+        User::factory(10)->create(['class_year_id' => ClassYearEnum::THIRD->value]);
+        User::factory(5)->create(['role_id' => RoleEnum::TEACHER->value]);
+        User::factory(30)->create(['role_id' => RoleEnum::PARENT->value]);
     }
 }
